92e5f907046796e1035bc9b1e2f2828cdce29190
Ramirez <maria.ramirez@softwareone.com>
2025-07-24 18:51:01 -0500

    Proyecto

--- src/string/StringMethodos.java
+++ src/string/StringMethodos.java
@@ -10,6 +10,12 @@ public class StringMethodos {
 		System.out.println("Contiene'mun'? " + saludo.contains("Mun"));
 		System.out.println("Remmplazo " + saludo.replace("Mundo", "Java"));
 		System.out.println(saludo.concat(" Bello"));
-		System.out.println("Termina en 'Hola'? "+ saludo.endsWith("Hola"));
+		System.out.println("Termina en 'Hola'? " + saludo.endsWith("Hola"));
+
+		String textUno = "Hola Mundo";
+		System.out.println(textUno.equals(saludo));
+		System.out.println("Indice de 'M' " + saludo.indexOf("Mundo"));
+		System.out.println("Indice de 'Mundo' " + saludo.indexOf("Mundo"));
+		System.out.println("Indice de 'Mundo' " + saludo.indexOf("Pete"));
 	}
 }
